88c3ee0b10e07e9098db5230b7ad4b5b7832a66d
Victor Njoroge <victornjoroge2020@gmail.com>
2023-07-27 03:48:07 -0700

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <>
+      <h1>Directors Page</h1>
+      {directors.map((index,director)=>(
+        <div key={index}>
+          <h3>Name: {director.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map((movie,index)=>(
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default Directors;
